cc4f8522376eb323e75004fcfe1e5d53d72a7039
Rachel S. Lee <rslee0025@gmail.com>
2020-06-09 00:20:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,5 +44,16 @@ def current_player
   turn_count.even? ? "X" : "O"
 end
 
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end
+end
 
 end 
